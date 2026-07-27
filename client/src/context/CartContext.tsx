eb71d2b701c9a8bb0918b6fbb293db174245bd0e
Mao Seavik <ahzarjy@Maos-MacBook-Pro.local>
2026-07-28 04:22:17 +0700

    Add product

--- client/src/context/CartContext.tsx
+++ client/src/context/CartContext.tsx
@@ -89,7 +89,7 @@ interface CartContextType {
 const CartContext = createContext<CartContextType | undefined>(undefined);
 
 const getApiBaseUrl = () => {
-  return "https://mini-app-2z1.pages.dev";
+  return "https://mini-app-mzu6.onrender.com";
 };
 const API_BASE_URL = getApiBaseUrl();
 
